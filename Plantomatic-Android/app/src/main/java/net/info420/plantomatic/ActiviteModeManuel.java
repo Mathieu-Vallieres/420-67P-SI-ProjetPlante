37bec4c1e43238044a148af66f4e41f2f6343aa4
ChristoV5565 <christophe.verreault@cegepsi.ca>
2023-03-28 19:25:31 -0400

    Création de toutes les activités

---
+++ Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActiviteModeManuel.java
@@ -0,0 +1,14 @@
+package net.info420.plantomatic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActiviteModeManuel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activitemodemanuel);
+    }
+}
