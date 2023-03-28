e5c629a96c4d06be65e9b4e65d832240d892f41a
ChristoV5565 <christophe.verreault@cegepsi.ca>
2023-03-28 18:09:48 -0400

    Ajout de layouts pour activités. Création de la toolbar

--- Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActivitePrincipale.java
+++ Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActivitePrincipale.java
@@ -1,7 +1,11 @@
 package net.info420.plantomatic;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toolbar;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class ActivitePrincipale extends AppCompatActivity {
@@ -10,5 +14,13 @@ public class ActivitePrincipale extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activiteprincipale);
+
+        //Préparation de la Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+        //Définit le titre "Plantomatic"
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(Color.WHITE);
+
     }
 }
