994c773e917304120e500de5a435a3de4a360bb7
ChristoV5565 <christophe.verreault@cegepsi.ca>
2023-03-29 19:28:02 -0400

    Création du menu de navigation

--- Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActivitePrincipale.java
+++ Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActivitePrincipale.java
@@ -1,26 +1,66 @@
 package net.info420.plantomatic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class ActivitePrincipale extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activiteprincipale);
 
-        //Préparation de la Toolbar
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setActionBar(toolbar);
-        //Définit le titre "Plantomatic"
-        toolbar.setTitle(R.string.app_name);
-        toolbar.setTitleTextColor(Color.WHITE);
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
 
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.menu_nav);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.ouvrirMenu, R.string.fermerMenu);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+/*        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.menu_nav:
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                return true;
+            }
+        });*/
     }
 }
