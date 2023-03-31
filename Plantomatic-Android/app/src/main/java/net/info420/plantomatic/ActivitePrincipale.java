7fbf2fbaf6024e0129ada37b2fbe7d6a81aadb12
ChristoV5565 <christophe.verreault@cegepsi.ca>
2023-03-31 16:21:24 -0400

    Création du layout pour l'activité principale
    Inclut image d'arrière-plan, liste, bouton, traduction en anglais

--- Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActivitePrincipale.java
+++ Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActivitePrincipale.java
@@ -1,18 +1,11 @@
 package net.info420.plantomatic;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.navigation.NavigationView;
