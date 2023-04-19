8211374219c87563fc9c2979bbcd18fc63d3089f
ChristoV5565 <christophe.verreault@cegepsi.ca>
2023-04-19 12:08:15 -0400

    Ajout du choix d'image
    L'activité détails permet maintenant de choisir la photo d'une plante à partir de la galerie présente sur le téléphone de l'utilisateur.

--- Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActiviteAffichage.java
+++ Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActiviteAffichage.java
@@ -1,5 +1,8 @@
 package net.info420.plantomatic;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,18 +10,32 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class ActiviteAffichage extends AppCompatActivity {
+public class ActiviteAffichage extends AppCompatActivity implements View.OnClickListener{
+
+    public final String TAG = "affichage";
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    Button boutonPhotoPlante;
+    ImageView imageViewPhoto;
 
     Intent intentAccueil;
     Intent intentDetails;
@@ -51,6 +68,11 @@ public class ActiviteAffichage extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.menu_nav);
+        boutonPhotoPlante = findViewById(R.id.boutonPhotoPlante);
+        imageViewPhoto = findViewById(R.id.imageViewPhoto);
+
+        boutonPhotoPlante.setOnClickListener(this);
+
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.ouvrirMenu, R.string.fermerMenu);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
@@ -85,5 +107,47 @@ public class ActiviteAffichage extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    //Permet de mettre la photo choisie dans l'imageview
+    @Override
+    public void onActivityResult(int requestCode, int Resultcode, Intent data) {
+        super.onActivityResult(requestCode, Resultcode, data);
+
+        if(requestCode == 1)
+        {
+            try {
+                imageViewPhoto.setImageURI(data.getData());
+            }
+            catch (Exception e)
+            {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+    }
+
+
+    //Listener pour les éléments clickables de l'interface
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
+            //Bouton qui permet de sélectionner une photo
+            case R.id.boutonPhotoPlante:
+
+                Log.d(TAG, "Choisir photo");
+
+                int code = 1;
+
+                //Création d'un intent qui invoquera un menu de sélection
+                Intent intentImage = new Intent();
+                //Seules les images pourront être sélectionnées
+                intentImage.setType("image/*");
+                intentImage.setAction(Intent.ACTION_GET_CONTENT);
+                //Démarrage de l'activité
+                startActivityForResult(intentImage, code);
+                break;
+        }
     }
 }
