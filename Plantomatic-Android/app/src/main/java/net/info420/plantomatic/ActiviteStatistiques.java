e3e47679e7e3aef0c6b7debb49bdcdce0c658417
Mathieu Vallières <mathieu.vallieres.etu@cegepsi.ca>
2023-04-19 18:13:58 -0400

    Mise en place de la communication MQTT à sens unique

--- Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActiviteStatistiques.java
+++ Plantomatic-Android/app/src/main/java/net/info420/plantomatic/ActiviteStatistiques.java
@@ -3,12 +3,47 @@ package net.info420.plantomatic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class ActiviteStatistiques extends AppCompatActivity {
+    MqttHelper mqttHelper;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activitestatistiques);
+        textView = findViewById(R.id.textView);
+        startMqtt();
+
     }
-}
+    private void startMqtt(){
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+                textView.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+}}
