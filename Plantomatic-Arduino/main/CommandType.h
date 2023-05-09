c1f8af1dde08e13fd8f08821e79b32c3064a2ef9
Romain Veydarier <106735422+Daril61@users.noreply.github.com>
2023-05-09 18:29:09 -0400

    Mise à jour de l'arduino
    Ajout du système d'arrosage automatique

--- Plantomatic-Arduino/main/CommandType.h
+++ Plantomatic-Arduino/main/CommandType.h
@@ -1,12 +1,18 @@
 enum CommandType {
   NONE,
   HUMIDITE,
-  ARROSER
+  ARROSER_ON,
+  ARROSER_OFF,
+  ARROSER_AUTO_ON,
+  ARROSER_AUTO_OFF
 };
 
 String CommandToString(CommandType type) {
   switch(type) {
     case HUMIDITE: return "HUMIDITE";
-    case ARROSER: return "ARROSER";
+    case ARROSER_ON: return "ARROSER_ON";
+    case ARROSER_OFF: return "ARROSER_OFF";
+    case ARROSER_AUTO_ON: return "ARROSER_AUTO_ON";
+    case ARROSER_AUTO_OFF: return "ARROSER_AUTO_OFF";
   }
 }
